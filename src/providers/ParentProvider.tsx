05c12f722aeb77e6395c731be49a7e3a6969e27c
Mason Secky-Koebel <mseckykoebel@gmail.com>
2024-10-02 19:32:06 -0500

    feat: add posthog

--- src/providers/ParentProvider.tsx
+++ src/providers/ParentProvider.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-// import { PostHogProvider } from "posthog-js/react";
+import { PostHogProvider } from "posthog-js/react";
 import { NextTamaguiProvider, ReactQueryProvider } from "./";
 
-// const apiKey = "phc_7Lg7HDoMB56ztjIAWBZN3YJ5vv8JDWNjiei6Dbn7wRO";
-// const options = {
-//   api_host: "https://app.posthog.com",
-// };
+const apiKey = "phc_i5hMAQU2CszpiwCq3i1gGoI2JJrEZaUBH1xDirL1aaH";
+const options = {
+  api_host: "https://app.posthog.com",
+};
 
 type Props = {
   children: React.ReactNode;
@@ -15,9 +15,9 @@ type Props = {
 export function ParentProvider({ children }: Props) {
   return (
     <ReactQueryProvider>
-      {/* <PostHogProvider apiKey={apiKey} options={options}> */}
-      <NextTamaguiProvider>{children}</NextTamaguiProvider>
-      {/* </PostHogProvider> */}
+      <PostHogProvider apiKey={apiKey} options={options}>
+        <NextTamaguiProvider>{children}</NextTamaguiProvider>
+      </PostHogProvider>
     </ReactQueryProvider>
   );
 }
